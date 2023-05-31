bed163c6bd6367ab117dfb9acfa3c0aef9c712aa
kaptencumi01 <adie.ndoyik@gmail.com>
2023-05-31 22:18:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# uts_adiprayitno_20220801459
+ Apps Wisata Indonesia
---
+++ android/app/src/main/java/com/example/uts_adiprayitno_20220801459/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.uts_adiprayitno_20220801459;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
